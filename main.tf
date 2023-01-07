8a4f4fbdd9a7a6ce353c43af5ede4a5431787f00
leoveranga <leoveranga@yahoo.com>
2023-01-07 12:07:46 -0500

    main.tf first commit

---
+++ main.tf
@@ -0,0 +1,77 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.90.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "enteryoursubscriptionidhere" 
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "learn-tf-rg-eastus"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "learn-tf-vnet-eastus"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "learn-tf-subnet-eastus"
+  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.main.name
+  address_prefixes     = ["10.0.0.0/24"]
+
+}
+
+
+#Creates network interface card (NIC)
+resource "azurerm_network_interface" "internal" {
+  name                = "learn-tf-nic-int-eastus"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+#Creates Virtual Machine
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "learntfvmeastus"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = "yourusername"
+  admin_password      = "your@securepass123"
+
+  network_interface_ids = [
+    azurerm_network_interface.internal.id
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-DataCenter"
+    version   = "latest"
+  }
+
+}
